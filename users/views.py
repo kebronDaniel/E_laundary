13afede19ffd0e96500489499f810f6451e7490e
Kebron <Kebronjrdaniel@gmail.com>
2020-11-03 15:39:41 +0300

    Added a feature to add a new product using form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProductCreateForm
 from django.contrib import messages
 
 
@@ -14,3 +14,18 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+def createProduct(request):
+    if request.method == 'POST':
+        p_form = ProductCreateForm(request.POST,request.FILES)
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request,f'A new product is created!')
+            return redirect('store')
+    else:
+        p_form = ProductCreateForm()
+        
+    return render(request,'users/create_product.html',{'p_form': p_form})
+
+
+
